3c73cf29acc5c327cf97d7a94533dca61db816e8
Fuji vanilli <ijufillivan@gmail.com>
2023-06-24 15:44:58 +0000

    first commit

---
+++ back/config-server/src/test/java/mg/marketmanagement/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.marketmanagement.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
